df4d91c9842c6805389899649f879265b6ca2eb9
Waldemar R <waldek.r@gmail.com>
2023-11-06 22:51:16 +0100

    update code

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by wrokita on 18/06/2017.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,8 +1,5 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +21,7 @@ public class SampleController {
         names.add("Zbigniew");
         names.add("Piotr");
 
-        Collections.sort(names,(n1, n2) -> n2.compareTo(n1));
+        Collections.sort(names, (n1, n2) -> n2.compareTo(n1));
         StringBuilder allNames = new StringBuilder();
         names.stream().forEach((item) -> allNames.append(item));
 
